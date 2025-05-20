f069eccd55d7ef08a62fd85c701819e1b275439c
DESKTOP-46K4QJ0\Dell <lp.nguyen0310@gmail.com>
2025-05-20 09:02:44 +0700

    Fix Rate limited Server send 5 request/ 60s

--- api-gateway/src/main/java/com/apigateway/apigateway/config/RateLimiterConfig.java
+++ api-gateway/src/main/java/com/apigateway/apigateway/config/RateLimiterConfig.java
@@ -1,8 +1,11 @@
 package com.apigateway.apigateway.config;
 
+import com.apigateway.apigateway.RateLimitedServer.FixedWindowRateLimiter;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RateLimiter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import reactor.core.publisher.Mono;
 
 @Configuration
@@ -10,12 +13,14 @@ public class RateLimiterConfig {
 
     @Bean
     public KeyResolver remoteAddrKeyResolver() {
-        return exchange -> {
-            String ip = exchange.getRequest()
-                    .getRemoteAddress()
-                    .getAddress()
-                    .getHostAddress();
-            return Mono.just(ip);
-        };
+        return exchange -> Mono.just(
+                exchange.getRequest().getRemoteAddress().getAddress().getHostAddress()
+        );
+    }
+
+    @Bean
+    @Primary
+    public RateLimiter<Object> fixedWindowRateLimiter() {
+        return new FixedWindowRateLimiter(5, 60); // 5 requests mỗi 60 giây
     }
 }
