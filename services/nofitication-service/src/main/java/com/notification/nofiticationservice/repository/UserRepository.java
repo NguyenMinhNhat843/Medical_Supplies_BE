203f8c01d17bf861f326a4316b2928813f5403df
DESKTOP-46K4QJ0\Dell <lp.nguyen0310@gmail.com>
2025-05-22 13:38:40 +0700

    update Service Fix CORS

--- services/nofitication-service/src/main/java/com/notification/nofiticationservice/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.notification.nofiticationservice.repository;
-
-import com.notification.nofiticationservice.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
