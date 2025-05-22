963d009904abd3248a497ab3be3f42bd337e8882
Trần Trọng Nhân-21095871 <nhanmpt000@gmail.com>
2025-05-22 20:47:29 +0700

    them logging cho order-service

--- services/order-service/src/main/java/com/order/orderservice/OrderServiceApplication.java
+++ services/order-service/src/main/java/com/order/orderservice/OrderServiceApplication.java
@@ -12,10 +12,6 @@ public class OrderServiceApplication {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
 }
 
 
